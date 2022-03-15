d24658203deb62c272e2def1c90d8c93b987bac0
Helen Raudsepp <helen.raudsepp@k-print.ee>
2022-03-15 19:47:55 +0200

    add new projects

---
+++ FirstOrLast/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace FirstOrLast
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm küsib kasutajal sisestada ees- ja perekonnanimed;
+            //programm kontrollib andmete pikkust eraldi 
+            //programm kuvab kumb nendest on pikem, kas ees- või perekonnanimi
+
+            Console.WriteLine("Palun sisesta oma eesnimi:");
+            string firstName = Console.ReadLine();
+            Console.WriteLine("Palun sisesta oma perekonnanimi:");
+            string lastName = Console.ReadLine();
+
+           
+
+            {
+                int firstNameLength = firstName.Length;
+                int lastNameLength = lastName.Length;
+
+                if (firstName.Length > lastName.Length)
+                {
+                    Console.WriteLine($"Sinu eesnimi on {firstNameLength} sümbolit ja seega pikem kui su perekonnanimi.");
+                }
+
+                else if (lastName.Length > firstName.Length)
+                {
+                    Console.WriteLine($"Sinu perekonnanimi on {lastNameLength} sümbolit ja seega pikem kui su eesnimi.");
+                }
+                else
+                {
+                    Console.WriteLine("Sinu ees- ja perekonnanimi on sama pikad!");
+                }
+            }
+        }
+    }
+}
---
+++ HOLCounter/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace HOLCounter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm kuvab konsoolis, mitu 'h', 'o' ja 'l' tähte on lauses "Hello World!";
+
+            Console.WriteLine("Sisesta sõnapaar: Hello World!");
+            string holCounter = Console.ReadLine().ToLower();
+
+            int hCounter = 0;
+
+            for(int i = 0; i < holCounter.Length; i++)
+            {
+                if(holCounter[i] == 'h')
+                { 
+                    hCounter++; 
+                }
+                    
+            }
+
+            int oCounter = 0;
+
+            for (int i = 0; i < holCounter.Length; i++)
+            {
+                if (holCounter[i] == 'o')
+                {
+                    oCounter++;
+                }
+                    
+            }
+
+            int lCounter = 0;
+
+            for (int i = 0 ; i < holCounter.Length; i++)
+            {
+                if (holCounter[i] == 'l')
+                {
+                    lCounter++;
+                }
+                    
+            }
+
+            Console.WriteLine($"Sinu lauses on {hCounter} 'h' tähte, {oCounter} 'o' tähte ja {lCounter} 'l' tähte.");
+        }
+    }
+}
